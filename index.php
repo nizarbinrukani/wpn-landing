1dcd4ab0e7619563554ba1086a0f0ba18a10ecab
Nizar Kurniawan <kurniawan.nizar.ploso@gmail.com>
2022-10-15 11:14:03 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <!-- Google Fonts CSS  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Nizar bin Rukani</title>
+  </head>
+  <body>
+    <!-- Navbar-begin -->
+    <header>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#">Nizar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+              <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="#">Feautures</a>
+              <a class="nav-link" href="#">About</a>
+              <a class="btn btn-primary tombol" href="#">Join Us</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- Navbar-end -->
+
+    <!-- Jumbotron-begin -->
+    <section id="jumbotron">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Get work down <span>faster</span> and <span>better</span> with us</h1>
+          <a class="btn btn-primary tombol" href="#">Our work</a>
+        </div>
+      </div>
+    </section>
+    <!-- Jumbotron-end -->
+
+    <!-- Panel-begin -->
+    <section id="panel">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10 info-panel">
+            <div class="row">
+              <div class="col-lg">
+                <img src="img/employee.png" alt="Employee" class="float-left">
+                <h4>24 Hours</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="col-lg">
+                <img src="img/hires.png" alt="Hires" class="float-left">
+                <h4>High-Res</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="col-lg">
+                <img src="img/security.png" alt="Security" class="float-left">
+                <h4>Security</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Workingspace begin -->
+        <div class="row workingspace">
+          <div class="col-md-6 col-sm-12">
+            <img src="img/workingspace.png" alt="Workingspace" class="img-fluid">
+          </div>
+          <div class="col-md-5 col-sm-12">
+            <h3>You <span>work</span> like at <span>home</span></h3>
+            <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+            <a href="" class="btn btn-primary tombol">Gallery</a>
+          </div>
+        </div>
+        <!-- Workingspace end -->
+      </div>  
+       
+      <!-- Testimonial begin -->
+      <section class="testimonial">
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <h5>" Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya. "</h5>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-6 justify-content-center d-flex">
+          <figure class="figure">
+              <img src="img/employee.png" class="figure-img img-fluid rounded-circle" alt="Testi 1">
+            </figure>
+            <figure class="figure">
+              <img src="img/employee.png" class="figure-img img-fluid rounded-circle utama" alt="Testi 2">
+              <figcaption class="figure-caption">
+                <h5>Sunny Ye</h5>
+                <p>Designer</p>
+              </figcaption>
+            </figure>
+            <figure class="figure">
+              <img src="img/employee.png" class="figure-img img-fluid rounded-circle" alt="Testi 3">
+            </figure>
+          </div>
+        </div>
+      </section>
+      <!-- Testimonial end -->
+    </section>
+    <!-- Panel-end -->
+
+    <!-- Footer begin -->
+    <footer id="footer" class="footer">
+      <div class="container">
+        <div class="row footer">
+          <div class="col text-center">
+            <p>2022 All Rights Reserved by Nizar.</p>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- Footer end -->
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
